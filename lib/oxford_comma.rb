6775dbf0f9d0554b7f26b1dd727e81a661abd773
Halie Hall <halierae17@outlook.com>
2021-04-13 19:09:02 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,10 +1,5 @@
 def oxford_comma(array)
-  if array.length == 2 
-    array.join(" and ")
-  elsif array.join(' and ') if .size < 3
-  list[-1] = "and " + list[-1]
-  list.join(', ')
-  else 
-    array.join
-  end
+   return array.join(' and ') if array.size < 3
+  array[-1] = "and " + array[-1]
+  array.join(', ')
 end
