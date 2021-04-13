4f277bb0c0571d99b88b6e8b1215b8809e66435d
Halie Hall <halierae17@outlook.com>
2021-04-13 19:04:46 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,6 +2,9 @@ def oxford_comma(array)
   if array.length == 2 
     array.join("and")
   elsif array.length > 2 
-  array.insert(-2, "and")
-  array[0..-1]
+    array.insert(-2, "and")
+    array[0..-1].join(", ")
+  else 
+    array.join
+  end
 end
